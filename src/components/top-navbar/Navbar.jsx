1987b2c44edf3d7611f4a274b704766c37f40f7c
fachrul fatanillah <87415039+fachrulfatanillah@users.noreply.github.com>
2025-04-23 20:52:59 +0700

    update sub-menu-navbar

--- src/components/top-navbar/Navbar.jsx
+++ src/components/top-navbar/Navbar.jsx
@@ -1,5 +1,6 @@
 import "./Navbar.css";
 import { useState } from "react";
+import { FaAngleRight } from 'react-icons/fa';
 
 import images_logo from "../../assets/logo/Chery_logo.png";
 import omoda_j6 from "../../assets/cars/omoda/OMODA-J6.webp";
@@ -14,44 +15,98 @@ import tiggo_8 from "../../assets/cars/tiggo/TIGGO-8.webp";
 import tiggo_cross from "../../assets/cars/tiggo/TIGGO-CROSS.webp";
 
 const Navbar = () => {
-    const [selectedImage, setSelectedImage] = useState(omoda_j6);
-
-    const handleImageChange = (model) => {
-        switch (model) {
-            // OMODA
-            case "J6":
-                setSelectedImage(omoda_j6);
-                break;
-            case "OMODA E5":
-                setSelectedImage(omoda_e5);
-                break;
-            case "OMODA 5 GT":
-                setSelectedImage(omoda_5_gt);
-                break;
-            case "OMODA 5":
-                setSelectedImage(omoda_5);
-                break;
-
-            // TIGGO
-            case "TIGGO CROSS":
-                setSelectedImage(tiggo_cross);
-                break;
-            case "TIGGO 8":
-                setSelectedImage(tiggo_8);
-                break;
-            case "TIGGO 8 PRO MAX":
-                setSelectedImage(tiggo_8_pro_max);
-                break;
-            case "TIGGO 8 PRO":
-                setSelectedImage(tiggo_8_pro);
-                break;
-            case "TIGGO 7 PRO":
-                setSelectedImage(tiggo_7_pro);
-                break;
-
-            default:
-                setSelectedImage(omoda_j6);
+    const carData = {
+        "J6": {
+            product_name : "J6",
+            image: omoda_j6,
+            specs: {
+                mesin: "1,5L CVT",
+                panjang: "4320 MM",
+                wheelbase: "2630 MM"
+            }
+        },
+        "OMODA E5": {
+            product_name : "OMODA E5",
+            image: omoda_e5,
+            specs: {
+                mesin: "1,6 TGDI",
+                panjang: "4722 MM",
+                wheelbase: "2710 MM"
+            }
+        },
+        "OMODA 5 GT": {
+            product_name : "OMODA 5 GT",
+            image: omoda_5_gt,
+            specs: {
+                mesin: "2,0 TGDI",
+                panjang: "4720 MM",
+                wheelbase: "2710 MM"
+            }
+        },
+        "OMODA 5": {
+            product_name : "OMODA 5",
+            image: omoda_5,
+            specs: {
+                mesin: "2,0 TGDI",
+                panjang: "4722 MM",
+                wheelbase: "2710 MM"
+            }
+        },
+        "TIGGO CROSS": {
+            product_name : "TIGGO CROSS",
+            image: tiggo_cross,
+            specs: {
+                mesin: "1,5 TCI",
+                panjang: "4500 MM",
+                wheelbase: "2670 MM"
+            }
+        },
+        "TIGGO 8": {
+            product_name : "TIGGO 8",
+            image: tiggo_8,
+            specs: {
+                mesin: "1,6 TGDI",
+                panjang: "4722 MM",
+                wheelbase: "2710 MM"
+            }
+        },
+        "TIGGO 8 PRO MAX": {
+            product_name : "TIGGO 8 PRO MAX",
+            image: tiggo_8_pro_max,
+            specs: {
+                mesin: "2,0 TGDI",
+                panjang: "4722 MM",
+                wheelbase: "2710 MM"
+            }
+        },
+        "TIGGO 8 PRO": {
+            product_name : "TIGGO 8 PRO",
+            image: tiggo_8_pro,
+            specs: {
+                mesin: "2,0 TGDI",
+                panjang: "4720 MM",
+                wheelbase: "2710 MM"
+            }
+        },
+        "TIGGO 7 PRO": {
+            product_name : "TIGGO 7 PRO",
+            image: tiggo_7_pro,
+            specs: {
+                mesin: "1,5 TCI",
+                panjang: "4500 MM",
+                wheelbase: "2670 MM"
+            }
         }
+    };
+    
+    const [selectedModel, setSelectedModel] = useState("J6");
+    const [selectedImage, setSelectedImage] = useState(carData["J6"].image);
+    const [spec, setSpec] = useState(carData["J6"].specs);
+    
+    const handleImageChange = (model) => {
+        setSelectedModel(model);
+        setSelectedImage(carData[model]?.image || carData["J6"].image);
+        setSpec(carData[model]?.specs || carData["J6"].specs);
     };
 
     return (
@@ -68,7 +123,45 @@ const Navbar = () => {
                         <div className="submenu-content">
                             {/* Left Section for Product Display */}
                             <div className="submenu-display-product">
-                                <img src={selectedImage} alt="model" />
+                                <div className="submenu-display-product-left">
+                                    <div className="submenu-display-product-box">
+                                        <strong>Mesin</strong>
+                                        {spec.mesin.split(" ").map((item, index) => (
+                                            <div key={`mesin-${index}`}>{item}</div>
+                                        ))}
+                                    </div>
+                                    <div className="submenu-display-product-box">
+                                        <strong>Panjang</strong>
+                                        {spec.panjang.split(" ").map((item, index) => (
+                                            <div key={`panjang-${index}`}>{item}</div>
+                                        ))}
+                                    </div>
+                                    <div className="submenu-display-product-box">
+                                        <strong>Wheelbase</strong>
+                                        {spec.wheelbase.split(" ").map((item, index) => (
+                                            <div key={`wheelbase-${index}`}>{item}</div>
+                                        ))}
+                                    </div>
+                                </div>
+                                <div className="submenu-display-product-right">
+                                    <img src={selectedImage} alt="model" className="product-image" />
+                                    <div className="submenu-detail-button-box">
+                                        <div className="detail-left-content">
+                                            {/* Bisa isi teks, info singkat, atau dikosongin */}
+                                        </div>
+                                        <div className="detail-right-button">
+                                            <button className="detail-button">
+                                                <div className="button-content">
+                                                    <div className="hexagon-icon"><FaAngleRight className="hexagon-icon-angle-right"/></div>
+                                                    <div className="button-text">
+                                                        <div className="product-name">{carData[selectedModel].product_name}</div>
+                                                        <div className="info-label">Detail Information</div>
+                                                    </div>
+                                                </div>
+                                            </button>
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
 
                             {/* Right Section for Categories */}
