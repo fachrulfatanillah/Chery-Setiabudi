e6c0fcae6804bb093cf4d7a7fdf01b878cb9f0a6
fachrul fatanillah <87415039+fachrulfatanillah@users.noreply.github.com>
2025-04-28 20:32:04 +0700

    create footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,5 +1,8 @@
 import "./Footer.css"
 
+import { FaPhoneAlt } from "react-icons/fa";
+import images_logo from "../../assets/logo/Chery_logo.png";
+
 const Footer = () => {
     return (
         <Container_Footer/>
@@ -9,7 +12,55 @@ const Footer = () => {
 const Container_Footer = () => {
     return (
         <div className="footer-section">
-
+            <div className="container-footer">
+                <div className="footer-title">
+                    <div className="footer-title-left">
+                        <img className="navbar-logo-image" src={images_logo} alt="" />
+                        <h1 className="navbar-logo-text">Chery-Setiabudi</h1>
+                    </div>
+                    <div className="footer-title-right">
+                        <div className="footer-title-right">
+                            <FaPhoneAlt className="phone-icon" />
+                            <a 
+                            className="phone-link" 
+                            href="https://wa.me/62881080248215" 
+                            target="_blank" 
+                            rel="noopener noreferrer"
+                            >
+                            +62 881‑0802‑48215
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div className="footer-detail">
+                    <div className="footer-detail-left">
+                        <div className="footer-detail-title">Kantor Pusat</div>
+                            <div className="footer-detail-address">
+                            <p>Jl. Wolter Monginsidi No.97, RT.1/RW.4, Rawa Barat, Kec. Kebayoran Baru, Kota Jakarta Selatan, Daerah Khusus Ibukota Jakarta 12180</p>
+                            <a 
+                                href="https://maps.google.com/?q=Jl. Wolter Monginsidi No.97, Jakarta" 
+                                target="_blank" 
+                                rel="noopener noreferrer"
+                                className="footer-link"
+                            >
+                                Lihat Lokasi
+                            </a>
+                        </div>
+                    </div>
+                    
+                    <div className="footer-detail-right">
+                        <div className="footer-detail-title">Model</div>
+                        <div className="footer-detail-links">
+                            <a href="#" className="footer-link">TIGGO</a>
+                            <a href="#" className="footer-link">OMODA</a>
+                        </div>
+                    </div>
+                </div>
+                <div className="footer-copy-right">
+                    © 2025 Chery-Setiabudi. All rights reserved.
+            </div>
+            </div>
+            
         </div>
     );
 }
