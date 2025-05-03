8bdfe85f9c68c5495c4fda248f91d140eeffb1f8
fachrul fatanillah <87415039+fachrulfatanillah@users.noreply.github.com>
2025-05-03 20:07:47 +0700

    change inline icon and contact emergency call

--- src/components/maintance/Maintenance.jsx
+++ src/components/maintance/Maintenance.jsx
@@ -8,16 +8,16 @@ function Maintenance() {
       <h1>Website Sedang Dalam Perawatan</h1>
       <p>Kami akan kembali dalam beberapa saat.</p>
 
-      <div className="emergency-contact">
-        <FaPhoneAlt className="phone-icon" />
-        <a 
-          href="https://wa.me/62881080248215" 
-          target="_blank" 
-          rel="noopener noreferrer"
-        >
-          +62 881‑0802‑48215
-        </a>
-      </div>
+        <div className="emergency-contact">
+            <FaPhoneAlt className="phone-icon" />
+            <a 
+            href="https://wa.me/62881080248215" 
+            target="_blank" 
+            rel="noopener noreferrer"
+            >
+            +62 881‑0802‑48215
+            </a>
+        </div>
     </div>
   )
 }
