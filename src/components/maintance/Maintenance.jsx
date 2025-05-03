2db2ab5c8174ccd83a6ab26bd0efa679a590d22d
fachrul fatanillah <87415039+fachrulfatanillah@users.noreply.github.com>
2025-05-03 19:30:18 +0700

    add responsive & maintance icon to maintance

--- src/components/maintance/Maintenance.jsx
+++ src/components/maintance/Maintenance.jsx
@@ -1,12 +1,14 @@
+import { FaTools } from 'react-icons/fa'
 import './Maintenance.css'
 
 function Maintenance() {
-    return (
-      <div style={{ textAlign: 'center', marginTop: '20%' }}>
-        <h1>🚧 Website Sedang Dalam Perawatan</h1>
-        <p>Kami akan kembali dalam beberapa saat.</p>
-      </div>
-    )
-  }
-  
-  export default Maintenance
+  return (
+    <div className="maintenance-container">
+      <FaTools className="maintenance-icon" />
+      <h1>Website Sedang Dalam Perawatan</h1>
+      <p>Kami akan kembali dalam beberapa saat.</p>
+    </div>
+  )
+}
+
+export default Maintenance
