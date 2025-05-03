f15b6659e6feac69b4cb7a75e55331be32910e02
fachrul fatanillah <87415039+fachrulfatanillah@users.noreply.github.com>
2025-05-03 19:49:32 +0700

    add emergency call maintance

--- src/components/maintance/Maintenance.jsx
+++ src/components/maintance/Maintenance.jsx
@@ -1,5 +1,5 @@
-import { FaTools } from 'react-icons/fa'
 import './Maintenance.css'
+import { FaTools, FaPhoneAlt } from 'react-icons/fa'
 
 function Maintenance() {
   return (
@@ -7,6 +7,17 @@ function Maintenance() {
       <FaTools className="maintenance-icon" />
       <h1>Website Sedang Dalam Perawatan</h1>
       <p>Kami akan kembali dalam beberapa saat.</p>
+
+      <div className="emergency-contact">
+        <FaPhoneAlt className="phone-icon" />
+        <a 
+          href="https://wa.me/62881080248215" 
+          target="_blank" 
+          rel="noopener noreferrer"
+        >
+          +62 881‑0802‑48215
+        </a>
+      </div>
     </div>
   )
 }
