1246bca960f95c495a9bfc4c71e05c89c2b1996e
fachrul fatanillah <87415039+fachrulfatanillah@users.noreply.github.com>
2025-05-03 19:22:15 +0700

    add maintance

---
+++ src/components/maintance/Maintenance.jsx
@@ -0,0 +1,12 @@
+import './Maintenance.css'
+
+function Maintenance() {
+    return (
+      <div style={{ textAlign: 'center', marginTop: '20%' }}>
+        <h1>🚧 Website Sedang Dalam Perawatan</h1>
+        <p>Kami akan kembali dalam beberapa saat.</p>
+      </div>
+    )
+  }
+  
+  export default Maintenance
